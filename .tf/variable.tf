c4d3cf4c3a385ef7f4f2b66d1c5d7cc0929b583f
Nam Pham <namph.hust@gmail.com>
2024-10-23 16:10:38 +0700

    Initial GKE ECK setup

---
+++ .tf/variable.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID of project to deploy this sample into"
+  default     = "Elastic Cluster"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP zone in which to deploy the GKE cluster"
+  default     = "us-central1"
+}
+variable "drain_job_name" {
+  type        = string
+  description = "Name for the drain job, this also affects the K8S namespace"
+  default     = "drain-job"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Machine type to use for nodes within the GKE node pools"
+  default     = "t2-standard-2"
+}
+
+variable "es_name" {
+  type        = string
+  description = "elasticsearch cluster pods label"
+  default     = "elasticsearch"
+}
